09c72d3cdc07cbd6dc3ea2fbcd7c39ed5c37a5be
Matthew Dippel <mdip226@gmail.com>
2022-09-19 13:21:22 -0400

    draft wallet page

---
+++ lib/routes/wallet.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+class WalletPage extends StatefulWidget {
+  const WalletPage({Key? key}) : super(key: key);
+
+  @override
+  State<WalletPage> createState() => _WalletPageState();
+}
+
+class _WalletPageState extends State<WalletPage> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(builder: builder)
+  }
+}
