57596e91241c4af09e0ba2b9e0069cd792fc0217
Matthew Dippel <mdip226@gmail.com>
2022-09-19 13:21:12 -0400

    load style from other app

---
+++ lib/utils/style.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+final themeData = ThemeData(
+  colorScheme: ColorScheme(
+      primary: Colors.blue.shade800,
+      secondary: Colors.blue.shade500,
+      surface: Colors.blue.shade300,
+      background: Colors.blue.shade300,
+      error: Colors.red,
+      onPrimary: Colors.blue.shade100,
+      onSecondary: Colors.blue.shade100,
+      onSurface: Colors.blue.shade800,
+      onBackground: Colors.blue.shade800,
+      onError: Colors.lightBlue,
+      brightness: Brightness.dark),
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(
+      primary: Colors.white,
+      backgroundColor: Colors.blue.shade900,
+      side: const BorderSide(
+        color: Colors.white,
+      ),
+    ),
+  ),
+  textTheme: TextTheme(
+    bodyText1: TextStyle(color: Colors.blue[900]),
+    bodyText2: TextStyle(
+      color: Colors.blue[900],
+      fontSize: 20,
+      overflow: TextOverflow.ellipsis,
+    ),
+  ),
+);
