b8203973e4c1b12aeecb8af912b6b96a86400196
Matthew Dippel <mdip226@gmail.com>
2022-09-19 13:21:41 -0400

    start of home page

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -12,6 +14,120 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text(''),),)
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('δημοκρατία'),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(7.0),
+            child: Image.asset(
+              "assets/justice.png",
+              height: 100,
+              width: 100,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(7.0),
+            child: RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                  text: "Welcome to ",
+                  style: Theme.of(context).textTheme.bodyText2,
+                  children: const <TextSpan>[
+                    const TextSpan(
+                      text: "Democracy",
+                      // style: _help
+                      //     ? const TextStyle(
+                      //         shadows: [
+                      //           Shadow(color: Colors.blueGrey, blurRadius: 49)
+                      //         ],
+                      //         fontWeight: FontWeight.bold,
+                      //         decoration: TextDecoration.underline,
+                      //       )
+                      //     : null,
+                    ),
+                    const TextSpan(text: "! If you are "),
+                    const TextSpan(
+                      text: "unfamiliar",
+                      // style: _help
+                      //     ? const TextStyle(
+                      //         shadows: [
+                      //           Shadow(color: Colors.blueGrey, blurRadius: 49)
+                      //         ],
+                      //         fontWeight: FontWeight.bold,
+                      //         decoration: TextDecoration.underline,
+                      //       )
+                      //     : null,
+                    ),
+                    const TextSpan(
+                        text: " with anything, or would just like to "),
+                    const TextSpan(
+                      text: "learn",
+                      // recognizer: TapGestureRecognizer()..onTap = _tapLearn,
+                      // style: _help
+                      //     ? const TextStyle(
+                      //         shadows: [
+                      //           Shadow(color: Colors.blueGrey, blurRadius: 49)
+                      //         ],
+                      //         fontWeight: FontWeight.bold,
+                      //         decoration: TextDecoration.underline,
+                      //       )
+                      //     : null,
+                    ),
+                    const TextSpan(text: " how this app "),
+                    const TextSpan(
+                      text: "works",
+                      // style: _help
+                      //     ? const TextStyle(
+                      //         shadows: [
+                      //           Shadow(color: Colors.blueGrey, blurRadius: 49)
+                      //         ],
+                      //         fontWeight: FontWeight.bold,
+                      //         decoration: TextDecoration.underline,
+                      //       )
+                      //     : null,
+                    ),
+                    const TextSpan(
+                        text:
+                            ", tap the question mark at the top right, then click on the underlined text that appears. If you're all set, tap the button below to begin "),
+                    const TextSpan(
+                      text: "the voting process",
+                      // style: _help
+                      //     ? const TextStyle(
+                      //         shadows: [
+                      //           Shadow(color: Colors.blueGrey, blurRadius: 49)
+                      //         ],
+                      //         fontWeight: FontWeight.bold,
+                      //         decoration: TextDecoration.underline,
+                      //       )
+                      //     : null,
+                    ),
+                    const TextSpan(text: "."),
+                  ]),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 20),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextButton(
+                style: Theme.of(context).textButtonTheme.style,
+                onPressed: () {},
+                child: const Text(
+                  "Get Started",
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
