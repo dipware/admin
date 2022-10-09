406477bc9226635dd1ede463ed1ed13e03e29d8e
Matthew Dippel <mdip226@gmail.com>
2022-10-08 20:45:00 -0400

    draft form

---
+++ lib/widgets/create_ballot_form.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/widgets.dart';
+
+class CreateBallotForm extends StatefulWidget {
+  const CreateBallotForm({Key? key}) : super(key: key);
+
+  @override
+  State<CreateBallotForm> createState() => _CreateBallotFormState();
+}
+
+class _CreateBallotFormState extends State<CreateBallotForm> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
