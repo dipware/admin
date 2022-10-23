dc180ac565deedef58693cbada7196510a63a121
Matthew Dippel <mdip226@gmail.com>
2022-10-23 12:32:27 -0400

    cleanup

--- lib/utils/wallet_storage.dart
+++ lib/utils/wallet_storage.dart
@@ -2,14 +2,10 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:math';
 
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:web3dart/web3dart.dart';
 
 class WalletStorage {
-  final _ethClient = Web3Client(dotenv.env['ETH_CLIENT']!, Client());
-
   Future<String> get _localWalletPath async {
     final directory = await getApplicationDocumentsDirectory();
     return '${directory.path}/wallets';
@@ -24,11 +20,6 @@ class WalletStorage {
     Wallet wallet = Wallet.createNew(credentials, password, random);
     return walletFile.writeAsString(wallet.toJson());
   }
-
-  // Future<File> loadWallet(String password) async {
-  //   final path = await _localWalletPath;
-  //   entities.forEach(print);
-  // }
 
   Future<List<File>> get wallets async {
     final path = await _localWalletPath;
