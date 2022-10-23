164593a929ab1d3497c9d67b15dbd1d91cf7956f
Matthew Dippel <mdip226@gmail.com>
2022-10-23 12:27:54 -0400

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,44 @@
-// import 'package:admin/routes/register.dart';
-import 'package:admin/providers/blockchain.dart';
-import 'package:admin/routes/voter_home.dart';
-import 'package:admin/routes/wallet_home.dart';
-import 'package:admin/routes/wallets_list.dart';
-import 'package:admin/utils/wallet_storage.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 
+import 'providers/blockchain.dart';
+import 'routes/voter_home.dart';
+import 'routes/wallet_home.dart';
+import 'routes/wallets_list.dart';
 import 'routes/home.dart';
 import 'routes/scan.dart';
+import 'utils/wallet_storage.dart';
 import 'utils/style.dart';
 
-import 'firebase_options.dart';
-import 'routes/register.dart';
-
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-  runApp(const VoterAdminApp());
+  runApp(const DemocracyApp());
 }
 
-class VoterAdminApp extends StatefulWidget {
-  const VoterAdminApp({Key? key}) : super(key: key);
+class DemocracyApp extends StatefulWidget {
+  const DemocracyApp({Key? key}) : super(key: key);
 
   @override
-  State<VoterAdminApp> createState() => _VoterAdminAppState();
+  State<DemocracyApp> createState() => _VoterAdminAppState();
 }
 
-class _VoterAdminAppState extends State<VoterAdminApp> {
-  // final Future<FirebaseApp> _initialization =
-  //     Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+class _VoterAdminAppState extends State<DemocracyApp> {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-        create: (context) => BlockChain(),
-        child: MaterialApp(
-          home: const HomePage(),
-          theme: themeData,
-          routes: {
-            ScanPage.routeName: (_) => ScanPage(),
-            WalletsListPage.routeName: (_) => WalletsListPage(
-                  walletStorage: WalletStorage(),
-                ),
-            VoterHomePage.routeName: (_) => VoterHomePage(),
-            // WalletHome.routeName: (_) => WalletHome(),
-            // RegisterPage.routeName: (_) => RegisterPage(),
-          },
-        ));
+      create: (context) => BlockChain(),
+      child: MaterialApp(
+        home: const HomePage(),
+        theme: themeData,
+        routes: {
+          WalletsListPage.routeName: (_) => WalletsListPage(
+                walletStorage: WalletStorage(),
+              ),
+          VoterHomePage.routeName: (_) => const VoterHomePage(),
+          WalletHome.routeName: (_) => const WalletHome(),
+        },
+      ),
+    );
   }
 }
